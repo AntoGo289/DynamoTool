42e7c60462bdeaf80c07757b93136ca65d34dcfe
Anto <antogo289@gmail.com>
2016-02-20 12:43:39 +0000

    Initial project start-up commit
    Initial project start-up commit

---
+++ DynamoToolAPI/Wrappers.cpp
@@ -0,0 +1,20 @@
+#include "Wrappers.h"
+
+Wrappers::Wrappers() {
+	// Constructer for Wrappers with setup
+};
+
+// Run script in current location
+void Wrappers::RunScriptHere() {
+
+};
+
+// Run script in current locatiom with arguments
+void Wrappers::RunScriptHere(string args[]) {
+
+};
+
+// Run script in current location with arguments and dependencies
+void Wrappers::RunScriptHere(string args[], string dependencies[]) {
+
+};
