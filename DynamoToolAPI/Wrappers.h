5d685a341270b69b7880f226a627e818b5f5914a
Anto <antogo289@gmail.com>
2016-02-20 15:18:45 +0000

    Simple wrapper functions with script name and args

--- DynamoToolAPI/Wrappers.h
+++ DynamoToolAPI/Wrappers.h
@@ -3,15 +3,17 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Wrappers
 {
 public:
 	Wrappers();
-	void RunScriptHere();
-	void RunScriptHere(string args[]);
-	void RunScriptHere(string args[], string dependencies[]);
+	void RunScriptHere(string scriptName);
+	void RunScriptHere(string scriptName, vector<string> args);
+	void RunScriptHere(string scriptName, vector<string> args, vector<string> dependencies);
 private:
 
 };
---
+++ DynamoToolAPI/test.bat
@@ -0,0 +1,6 @@
+dir
+set arg1=%1
+set arg2=%2
+echo %arg1%
+echo %arg2%
+TIMEOUT /T 10
